b8f044b1432020ebb0a914f2c9fc88f93a6a67b9
Luke_Young <lukeyoung3000@gmail.com>
2019-05-10 18:51:43 +1000

    First Commit

---
+++ VCNL4035_Lib.cpp
@@ -0,0 +1,125 @@
+/**
+ * @file    ZX_Sensor.cpp
+ * @brief   Library for the SparkFun/GestureSense ZX Sensor
+ * @author  Shawn Hymel (SparkFun Electronics)
+ *
+ * @copyright	This code is public domain but you buy me a beer if you use
+ * this and we meet someday (Beerware license).
+ *
+ * This library interfaces the XYZ Interactive ZX Sensor to Arduino over I2C.
+ * The library relies on the Arduino Wire (I2C) library. To use the library,
+ * instantiate a ZX_Sensor object, call init(), and call the desired
+ * functions.
+ */
+ 
+#include <Arduino.h>
+#include <Wire.h>
+
+
+#include "VCNL4035_Lib.h"
+
+
+uint16_t VCNL3045::readVersion()
+{
+	uint16_t ver = 0;
+	//wireReadReg(VISHAY_VERSION_REG, ver);
+	ver = wireReadReg_2(VISHAY_VERSION_REG);
+	
+	#if DEBUG
+	Serial.print("VCNL4035 Version: ");
+	Serial.println(ver,HEX);
+	#endif
+	
+	return ver;
+}
+
+
+/*******************************************************************************
+ * Raw I2C Reads and Writes
+ * Taken from the ZX_Sensor library from Spark-fun and Modified
+ ******************************************************************************/
+
+/**
+ * @brief Writes a single byte to the I2C device (no register)
+ *
+ * @param[in] val the 1-byte value to write to the I2C device
+ * @return True if successful write operation. False otherwise.
+ */
+bool VCNL4035::wireWriteByte(uint8_t val)
+{
+    Wire.beginTransmission(addr_);
+    Wire.write(val);
+    if( Wire.endTransmission(false) != 0 ) {
+        return false;
+    }
+    
+    return true;
+}
+
+/**
+ * @brief Writes two bytes to the I2C device and specified register
+ *
+ * @param[in] reg the register in the I2C device to write to
+ * @param[in] low_byte is the lower register bit
+ * @param[in] high_byte is the higher register bit
+ * @return True if successful write operation. False otherwise.
+ */
+bool VCNL4035::wireWriteReg(uint8_t reg, uint8_t low_byte, uint8_t high_byte)
+{
+    Wire.beginTransmission(addr_);
+    Wire.write(reg);
+    Wire.write(low_byte);
+    Wire.write(high_byte);
+    if( Wire.endTransmission(false) != 0 ) {
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * @brief Reads a two bytes from the I2C device and specified register
+ *
+ * @param[in] reg the register to read from
+ * @param[out] val the value returned from the register
+ * @return True if successful read operation. False otherwise.
+ */
+bool VCNL4035::wireReadReg(uint8_t reg, uint16_t &val)
+{
+	uint16_t bytes[2] = {0,0};
+	uint8_t i = 0;
+
+	
+    /* Indicate which register we want to read from */
+    if (!wireWriteByte(reg)) {
+        return false;
+    }
+    
+    /* Read from register */
+    Wire.requestFrom(addr_, uint8_t(2));
+    while (Wire.available()) {
+        // Possible over flow errors here
+        bytes[i] = Wire.read();
+		    i++;
+    }
+    
+	bytes[1] = (bytes[1] << 8) + bytes[0];
+	val = bytes[1];
+	
+    return true;
+}
+
+uint16_t VCNL4035::wireReadReg_2(uint8_t reg)
+{
+  uint16_t reading;
+  Wire.beginTransmission(addr_);
+  Wire.write(reg);
+  Wire.endTransmission(false);
+  Wire.requestFrom(addr_, uint8_t(2));
+  while (!Wire.available());
+  uint8_t byteLow = Wire.read();
+  while (!Wire.available());
+  uint16_t byteHigh = Wire.read();
+  reading = (byteHigh <<= 8) + byteLow;
+  return reading;
+}
---
+++ VCNL4035_Lib.h
@@ -0,0 +1,62 @@
+/**
+ * @file    VCNL4035_Lib.h
+ * @brief   Library for the VCNL3045
+ * @author  Luke Young (L S Young Electrical)
+ *
+ * @copyright	This code is public domain but you buy me a beer if you use
+ * this and we meet someday (Beerware license).
+ *
+ * This library interfaces the XYZ Interactive ZX Sensor to Arduino over I2C.
+ * The library relies on the Arduino Wire (I2C) library. To use the library,
+ * instantiate a ZX_Sensor object, call init(), and call the desired
+ * functions.
+ */
+ 
+#ifndef VCNL4035_Lib_H
+#define VCNL4035_Lib_H
+
+#include <Arduino.h>
+
+/* Debug */
+#define DEBUG               1
+
+/* Acceptable VCNL3045 version */
+// Add version numbers!!
+
+/* VCNL3045 I2C slave address */
+#define VISHAY_I2C_ADDR   		0x60
+
+/* VCNL3045 register addresses */
+#define PS_CONF_1_2   			0x03
+#define PS_CONF_3_MS  			0x04
+#define RES_INT_FLAGS 			0x0D
+#define PS1_DATA_REG  			0x08
+#define PS2_DATA_REG  			0x09
+#define PS3_DATA_REG  			0x0A
+#define VISHAY_VERSION_REG 		0x0E
+
+
+
+/* ZX Sensor Class */
+class VCNL4035 {
+public:
+
+	/* Bit manipulation */
+	// bool setRegisterBit(uint8_t reg, uint8_t msb_lsb, uint8_t bit);
+	// bool clearRegisterBit(uint8_t reg, uint8_t msb_lsb, uint8_t bit);
+	// bool writeRegisterBits(uint8_t reg, uint8_t msb_lsb, \
+							uint8_t start_bit, uint8_t end_bit, uint8_t val);
+							
+	/* Raw I2C reads and writes */
+    bool wireWriteByte(uint8_t val);
+	bool wireWriteReg(uint8_t reg, uint8_t low_byte, uint8_t high_byte);
+	bool wireReadReg(uint8_t reg, uint16_t &val);
+	uint16_t wireReadReg_2(uint8_t reg);
+	
+private:
+
+	uint8_t addr_ = VISHAY_I2C_ADDR;
+
+};
+
+#endif /* VCNL4035_Lib_H */
